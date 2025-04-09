2add39ab9f5dd530f83e8dcf79c92605342773b6
Irfan Hakeem <hakeemkhairis2006@gmail.com>
2025-04-09 16:02:12 +0800

    clean the print line

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, WMYVIIIIIbksbmjasb,ja,jda,jadbcsII orld!");
+        System.out.println("Hello, WMYVIII");
     }
 }
