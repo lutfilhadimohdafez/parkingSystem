14b8520fbc848fb024aefc52384004755ba7e9f2
Haikal <horob184@gmail.com>
2025-04-26 01:11:28 +0800

    Haikal added plate function but no back function

---
+++ src/Platenumber/plate.java
@@ -0,0 +1,53 @@
+package Platenumber;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class plate {
+
+
+    public plate() {
+        JFrame plate = new JFrame();
+        plate.setResizable(false);
+        JLayeredPane plateLayer = new JLayeredPane();
+        plateLayer.setPreferredSize(new Dimension(500, 300));
+        plate.setTitle("Plate Number");
+        plate.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        plate.setLocationRelativeTo(null);
+        plate.setSize(500,500);
+        plate.setVisible(true);
+
+
+        JLabel PlateLabel = new JLabel("Enter Your Plate Number:");
+        PlateLabel.setBounds(50, 50, 150, 30);
+
+        JTextField textPlate = new JTextField();
+        textPlate.setBounds(200, 50, 200, 30);
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(200, 100, 100, 30);
+        submitButton.addActionListener(e -> {
+            String PlateID = textPlate.getText();
+            System.out.println("Plate Number: " + PlateID);
+            JOptionPane.showConfirmDialog(null, "Do you confirm your plate is " + PlateID, "Confirmation", JOptionPane.YES_NO_OPTION);
+        });
+
+        // jangan lupa code for main menu back button
+        JButton back = new JButton("Back");
+        back.setBounds(200, 150, 100, 30);
+
+        
+
+        plateLayer.add(PlateLabel);
+        plateLayer.add(textPlate);
+        plateLayer.add(submitButton);
+        plateLayer.add(back);
+        plate.add(plateLayer);
+    
+
+
+        
+        
+    }
+}
